8a796709bd337bf6a524d7814434056586b7cdac
Max Rothman <max@edx.org>
2017-02-15 19:21:28 -0500

    Initial work

---
+++ tests/test_trie.py
@@ -0,0 +1,166 @@
+import unittest
+from wordsearch.trie import TrieNode
+
+def recursive_equal(first, second):
+  """
+  Return True if the tree rooted by "first" is identical to the tree rooted by
+  "second", i.e. all the nodes and edges are identical.
+  """
+  first_queue = [first]
+  second_queue = [second]
+  while first_queue and second_queue:
+    first_item = first_queue.pop()
+    second_item = second_queue.pop()
+    if first_item != second_item:
+      return False
+
+    first_queue.extend(sorted(first_item.children.values(), key=lambda x: x.letter))
+    second_queue.extend(sorted(second_item.children.values(), key=lambda x: x.letter))
+
+  if len(first_queue) != len(second_queue):
+    return False
+
+  return True
+
+class TestRecursiveEqual(unittest.TestCase):
+  def test_equal(self):
+    self.assertTrue(recursive_equal(
+      TrieNode(words=['amp', 'ack', 'bus']), TrieNode(words=['amp', 'ack', 'bus'])
+    ))
+
+  def test_not_equal(self):
+    self.assertFalse(recursive_equal(
+      TrieNode(words=['amp', 'ack', 'bus']), TrieNode(words=['amm', 'ack', 'bus'])
+    ))
+    self.assertFalse(recursive_equal(
+      TrieNode(words=['am', 'ac', 'bus']), TrieNode(words=['amm', 'ack', 'bus'])
+    ))
+
+
+class TestTrie(unittest.TestCase):
+  def setUp(self):
+    self.root_node = TrieNode(children=[
+      TrieNode('a', children=[
+        TrieNode('m', children=[
+          TrieNode('p', word_end=True)
+        ]),
+        TrieNode('c', children=[
+          TrieNode('k', word_end=True)
+        ])
+      ]),
+      TrieNode('b', children=[
+        TrieNode('u', children=[
+          TrieNode('s', word_end=True)
+        ])
+      ])
+    ])
+
+  def test_root(self):
+    root = TrieNode()
+    self.assertEqual(root.children, {})
+    self.assertEqual(root.letter, None)
+
+  def test_equals(self):
+    self.assertEqual(TrieNode(), TrieNode())
+    self.assertEqual(TrieNode('a'), TrieNode('a'))
+    self.assertEqual(TrieNode(children=[TrieNode('a')]), TrieNode(children=[TrieNode('a')]))
+    self.assertEqual(TrieNode('a', children=[TrieNode('b')]), TrieNode('a', children=[TrieNode('b')]))
+    self.assertEqual(TrieNode('a', word_end=True), TrieNode('a', word_end=True))
+
+  def test_not_equals(self):
+    self.assertNotEqual(TrieNode(), TrieNode('a'))
+    self.assertNotEqual(TrieNode(), TrieNode(children=[TrieNode('a')]))
+    self.assertNotEqual(TrieNode('a'), TrieNode('b'))
+    self.assertNotEqual(TrieNode(children=[TrieNode('a')]), TrieNode(children=[TrieNode('b')]))
+    self.assertNotEqual(TrieNode('c', children=[TrieNode('a')]), TrieNode('d', children=[TrieNode('a')]))
+    self.assertNotEqual(TrieNode('c', children=[TrieNode('a')]), TrieNode('c', children=[TrieNode('b')]))
+    self.assertNotEqual(TrieNode('a'), TrieNode('a', word_end=True))
+
+  def test_child(self):
+    root = TrieNode(children=[TrieNode('a')])
+    self.assertEqual(root.letter, None)
+    self.assertTrue('a' in root.children)
+    self.assertEqual(root.children['a'], TrieNode('a'))
+
+  def test_none_in_children(self):
+    self.assertRaises(ValueError, lambda: TrieNode(children=[TrieNode()]))
+  
+  def test_lowers_letter(self):
+    self.assertEqual(TrieNode('A'), TrieNode('a'))
+
+  def test_only_one_letter(self):
+    self.assertRaises(ValueError, lambda: TrieNode('ab'))
+
+  def test_init_children_or_words(self):
+    self.assertRaises(ValueError, lambda: TrieNode(children=[TrieNode('a')], words=['b']))
+    
+    try:
+      TrieNode(children=[TrieNode('a')])
+    except ValueError:
+      self.fail("Should not get a ValueError when building TrieNode with only children.")
+    
+    try:
+      TrieNode(words=['foo'])
+    except ValueError:
+      self.fail("Should not get a ValueError when building TrieNode with only words")
+
+    try:
+      TrieNode()
+    except ValueError:
+      self.fail("Should not get a ValueError when building TrieNode with no children or words")
+
+  def test_index(self):
+    root = TrieNode()
+    root.index('amp', 'ack', 'bus')
+    
+    self.assertTrue(recursive_equal(root, self.root_node))
+
+  def test_construct_with_words(self):
+    root = TrieNode(words=['amp', 'ack', 'bus'])
+    self.assertEqual(root, self.root_node)
+
+  def test_construct_with_words_other_iterator(self):
+    root = TrieNode(words={'amp', 'ack', 'bus'})
+    self.assertEqual(root, self.root_node)
+
+  def test_construct_empty_wordlist(self):
+    self.assertEqual(TrieNode(words=[]), TrieNode())
+
+  def test_full_does_contain(self):
+    self.assertTrue(self.root_node.contains('amp'))
+    self.assertTrue(self.root_node.contains('ack'))
+    self.assertTrue(self.root_node.contains('bus'))
+
+  def test_partial_does_contain(self):
+    self.assertFalse(self.root_node.contains('a'))
+    self.assertFalse(self.root_node.contains('ac'))
+    self.assertFalse(self.root_node.contains('bu'))
+
+  def test_partial_does_contain_prefix(self):
+    self.assertTrue(self.root_node.contains('a', prefix=True))
+    self.assertTrue(self.root_node.contains('ac', prefix=True))
+    self.assertTrue(self.root_node.contains('bu', prefix=True))
+
+  def test_does_not_contain(self):
+    self.assertFalse(self.root_node.contains('car'))
+    self.assertFalse(self.root_node.contains('candy'))
+    self.assertFalse(self.root_node.contains('amd'))
+    self.assertFalse(self.root_node.contains('adc'))
+    self.assertFalse(self.root_node.contains('bur'))
+    self.assertFalse(self.root_node.contains('apple'))
+
+  def test_dunder_contains(self):
+    self.assertTrue('amp' in self.root_node)
+    self.assertFalse('a' in self.root_node)
+    self.assertFalse('car' in self.root_node)
+
+  def test_repr(self):
+    self.assertEqual(
+      repr(TrieNode('a', children=[TrieNode('b'), TrieNode('c')])),
+      "TrieNode(letter=a, children={b, c}, word_end=False)"
+    )
+
+  def test_add_chilren(self):
+    root = TrieNode()
+    root._add_children(TrieNode('a'))
+    self.assertTrue('a' in root.children)
---
+++ wordsearch/trie.py
@@ -0,0 +1,129 @@
+class TrieNode:
+  """
+  A node in a trie.
+
+  Note that "children" is not intended for modification. If you want to directly add
+  children to a node, use "add_children()".
+
+  Usage examples:
+    >>> root = TrieNode(words=['foo', 'bar', 'baz'])
+    >>> 'bing' in root
+    False
+    >>> root.index('bing')
+    >>> 'bing' in root
+    True
+    >>> root.children
+    {'b': TrieNode(letter=b, children={a, i}), 'f': TrieNode(letter=f, children={o})}
+  """
+  def __init__(self, letter=None, words=None, children=None, word_end=False):
+    """
+    Args:
+      letter: the letter this node represents, a string of length 1 or None.
+              None indicates that this node is the root of the Trie.
+      words: an iterable of words to index. Shortcut for iteratively calling TrieNode.index()
+      children: an iterable of TrieNodes to become this node's children
+      word_end: whether this node is the end of a word.
+    
+    "words" and "children" are mutually exclusive; providing both will raise a ValueError.
+    """
+    self.word_end = word_end
+
+    if letter is None:
+      pass
+    elif len(letter) != 1:
+      raise ValueError('"letter" should have a length of 1')
+    else:
+      letter = letter.lower()
+    self.letter = letter
+
+    self.children = {}
+
+    if words is not None and children is not None:
+      raise ValueError('Arguments "words" and "children" are mutually exclusive')
+    elif words is not None:
+      for word in words:
+        self.index(word)
+    elif children is not None:
+      self._add_children(*children)
+
+
+  def index(self, *words):
+    """
+    Add all the "words" args to the trie
+
+    Args:
+      words: strings of words to add to the trie under this node.
+    """
+    if self.letter is not None:
+      raise ValueError('{}.index() should only be called on the root node'.format(
+        type(self).__name__
+      ))
+
+    for word in words:
+      cur_node = self
+      for letter in word:
+        if letter in cur_node.children:
+          new_node = cur_node.children[letter]
+        else:
+          new_node = type(self)(letter)
+          cur_node._add_children(new_node)
+      
+        cur_node = new_node
+      
+      cur_node.word_end = True
+
+
+  def _add_children(self, *nodes):
+    """
+    Add children to this node
+
+    Args:
+      nodes: TrieNodes to become this node's children
+    """
+    if any(child.letter is None for child in nodes):
+      raise ValueError("Only the root node should have letter == None")
+    self.children.update({child.letter: child for child in nodes})
+
+
+  def contains(self, word, prefix=False):
+    """
+    Return True if the trie contains "word". By default only matches whole words, but
+    if "prefix" is True, then also return True if the trie contains the prefix
+    "word".
+
+    Note that this intentionally skips the current node's letter since the root node
+    has no letter.
+    """
+    cur_node = self
+    for letter in word:
+      if letter in cur_node.children:
+        cur_node = cur_node.children[letter]
+      else:
+        return False
+
+    # If the current node doesn't end a word, return False unless prefix==True
+    return cur_node.word_end or prefix
+
+
+  def __contains__(self, word):
+    """
+    Shortcut for TrieNode.contains(word, prefix=False). Overloads "in" operator. 
+    """
+    return self.contains(word)
+
+
+  def __eq__(self, other):
+    """
+    Return True if "self" has the same letter and children as "other". Overloads the
+    "==" operator.
+    """
+    return all(getattr(self, itm) == getattr(other, itm) for itm in ['letter', 'children', 'word_end'])
+
+
+  def __repr__(self):
+    return "{}(letter={}, children={{{}}}, word_end={})".format(
+      type(self).__name__,
+      self.letter,
+      ', '.join(self.children.keys()),
+      self.word_end,
+    )
